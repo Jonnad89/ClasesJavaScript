bf054226790add5bf07659170239b9bf742516ac
Jonnad89 <jonnadvwork@gmail.com>
2024-11-22 16:07:21 -0300

    clase 4

---
+++ Ejercicios segunda semana/ejercicio1.js
@@ -0,0 +1,8 @@
+/*
+
+Crea un objeto llamado auto con las propiedades marca, modelo y año.
+Agrega un método detalles que imprima todas las propiedades en formato de cadena.
+Crea otro objeto autoDeportivo que herede de auto y agrega la propiedad velocidadMaxima.
+Llama al método detalles desde autoDeportivo y muestra su información junto con velocidadMaxima.
+
+*/
