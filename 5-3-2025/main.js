04f24c291e619d01e63af84bf708495eb96c504a
Jonnad89 <jonnadvwork@gmail.com>
2025-03-05 16:11:15 -0300

    localStorage, SessionStorage y Cookies

---
+++ 5-3-2025/main.js
@@ -0,0 +1,13 @@
+
+
+// Obtener el valor del contador desde LocalStorage
+// let visitas =localStorage.getItem("visitas") || 0;
+
+//convertir a número y aumentar en 1
+// visitas = parseInt(visitas) +1;
+
+//guardar en localStorage
+// localStorage.setItem("visitas", visitas);
+
+// mostrar en la página
+// document.getElementById("contador").textContent = visitas;
